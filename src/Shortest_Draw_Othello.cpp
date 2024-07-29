88a159e026b6af214eb071120ef866c6c8f4834d
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-07-29 19:42:39 +0900

    refactoring

--- src/Shortest_Draw_Othello.cpp
+++ src/Shortest_Draw_Othello.cpp
@@ -13,42 +13,10 @@
 #include "engine/board.hpp"
 #include "engine/util.hpp"
 
-#define N_SEEN_LINES 10
-
-constexpr uint64_t lines_dir[N_SEEN_LINES] = {
-    // horizontal
-    0x00000000000000FFULL, // a8-h8
-    0x000000000000FF00ULL, // a7-h7
-    0x0000000000FF0000ULL, // a6-h6
-    0x00000000FF000000ULL, // a5-h5
-    // diagonal 9
-    //0x0000000000008040ULL, // a7-b8
-    0x0000000000804020ULL, // a6-c8 done
-    0x0000000080402010ULL, // a5-d8 done
-    0x0000008040201008ULL, // a4-e8
-    0x0000804020100804ULL, // a3-f8
-    0x0080402010080402ULL, // a2-g8
-    0x8040201008040201ULL,  // a1-h8
-    // corner
-    //0x0000000000000080ULL, // a8
-};
-
-std::vector<uint64_t> lines;
-
-void lines_init(){
-    for (int i = 0; i < N_SEEN_LINES; ++i){
-        lines.emplace_back(lines_dir[i]);
-        lines.emplace_back(rotate_90(lines_dir[i]));
-        lines.emplace_back(rotate_180(lines_dir[i]));
-        lines.emplace_back(rotate_270(lines_dir[i]));
-    }
-}
-
 void init(){
     bit_init();
     mobility_init();
     flip_init();
-    lines_init();
 }
 
 void output_transcript(std::vector<int> &transcript){
@@ -377,15 +345,6 @@ bool check_all_connected(uint64_t discs){
     return visited == discs;
 }
 
-bool check_full_lines(uint64_t discs){
-    for (uint64_t &line: lines){
-        if ((discs & line) == line){
-            return true;
-        }
-    }
-    return false;
-}
-
 // generate silhouettes
 void generate_silhouettes(uint64_t discs, int depth, uint64_t seen_cells, uint64_t *n_silhouettes, uint64_t *n_boards, uint64_t *n_solutions, std::unordered_set<uint64_t> &seen_unique_discs, bool connected, int max_memo_depth){
     //if (check_full_lines(discs)){ // full lines already seen
@@ -458,7 +417,6 @@ int main(int argc, char* argv[]){
     const std::vector<Task> tasks = {
         {0x8040201008040201ULL | 0x0000001818000000ULL, 20}, // a1-h8 (4 solutions found at depth 20)
         {0x0000000000FF0000ULL | 0x0000001818000000ULL, 20}, // a6-h6 (4 solutions (same as a1-h8 lines) found at depth 20)
-        /*
         {0x00000000FF000000ULL | 0x0000001818000000ULL, 20}, // a5-h5
         {0x0080402010080402ULL | 0x0000001818000000ULL, 20}, // a2-g8
         {0x0000804020100804ULL | 0x0000001818000000ULL, 20}, // a3-f8
@@ -469,7 +427,6 @@ int main(int argc, char* argv[]){
         {0x0000000000804020ULL | 0x0000001818000000ULL, 18}, // a6-c8
         {0x0000000020408070ULL | 0x0000001818000000ULL, 20}, // a7-b8, b6-c5, c8-d8
         {0x0000000080808070ULL | 0x0000001818000000ULL, 20}  // a7-b8, a5-a6, c8-d8
-        */
     };
     uint64_t strt = tim();
     for (int depth = 4; depth <= 20; depth += 2){
