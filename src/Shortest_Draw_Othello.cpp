8fc189227b200f5dd535d4b6e35585800b68bcbf
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-08-02 02:24:06 +0900

    updating main code

--- src/Shortest_Draw_Othello.cpp
+++ src/Shortest_Draw_Othello.cpp
@@ -22,6 +22,7 @@ void init(){
 }
 
 void output_transcript(std::vector<int> &transcript){
+    std::cout << "SOLUTION ";
     for (int &move: transcript){
         std::cout << idx_to_coord(move);
     }
@@ -368,9 +369,8 @@ void generate_boards(Board *board, uint64_t discs, int n_discs_half, uint64_t *n
 
 // generate draw endgame
 void generate_boards(Board *board, std::vector<uint64_t> &groups, int group_idx, int n_discs_half, uint64_t *n_boards, uint64_t *n_solutions){
-    uint64_t board_discs = board->player | board->opponent;
-    if (group_idx > groups.size()){
-        if (board->is_end() && board->score_player() == 0){ // game over with draw
+    if (group_idx >= groups.size()){
+        if (board->is_end() && board->score_player() == 0){ // game over
             ++(*n_boards);
             find_path(board, n_solutions);
         }
@@ -378,16 +378,21 @@ void generate_boards(Board *board, std::vector<uint64_t> &groups, int group_idx,
     }
     int n_player = pop_count_ull(board->player);
     int n_opponent = pop_count_ull(board->opponent);
-    if (n_player > n_discs_half || n_opponent > n_discs_half){
-        return;
-    }
+    //if (n_player > n_discs_half || n_opponent > n_discs_half){
+    //    return;
+    //}
     uint64_t group = groups[group_idx];
-    board->player ^= group;
-        generate_boards(board, groups, group_idx + 1, n_discs_half, n_boards, n_solutions);
-    board->player ^= group;
-    board->opponent ^= group;
-        generate_boards(board, groups, group_idx + 1, n_discs_half, n_boards, n_solutions);
-    board->opponent ^= group;
+    int n_group = pop_count_ull(group);
+    if (n_player + n_group <= n_discs_half){
+        board->player ^= group;
+            generate_boards(board, groups, group_idx + 1, n_discs_half, n_boards, n_solutions);
+        board->player ^= group;
+    }
+    if (n_opponent + n_group <= n_discs_half && group_idx > 0){
+        board->opponent ^= group;
+            generate_boards(board, groups, group_idx + 1, n_discs_half, n_boards, n_solutions);
+        board->opponent ^= group;
+    }
 }
 
 uint64_t get_neighbour_discs(uint64_t discs){
@@ -537,7 +542,6 @@ void generate_silhouettes(uint64_t discs, int depth, uint64_t seen_cells, uint64
         return;
     }
     uint64_t neighbours = 0;
-    
     neighbours |= ((discs & 0x7F7F7F7F7F7F7F7FULL) << 1) & ((discs & 0x3F3F3F3F3F3F3F3FULL) << 2);
     neighbours |= ((discs & 0xFEFEFEFEFEFEFEFEULL) >> 1) & ((discs & 0xFCFCFCFCFCFCFCFCULL) >> 2);
     neighbours |= ((discs & 0x00FFFFFFFFFFFFFFULL) << 8) & ((discs & 0x0000FFFFFFFFFFFFULL) << 16);
@@ -546,18 +550,14 @@ void generate_silhouettes(uint64_t discs, int depth, uint64_t seen_cells, uint64
     neighbours |= ((discs & 0x7F7F7F7F7F7F7F00ULL) >> 7) & ((discs & 0x3F3F3F3F3F3F0000ULL) >> 14);
     neighbours |= ((discs & 0x007F7F7F7F7F7F7FULL) << 9) & ((discs & 0x00003F3F3F3F3F3FULL) << 18);
     neighbours |= ((discs & 0xFEFEFEFEFEFEFE00ULL) >> 9) & ((discs & 0xFCFCFCFCFCFC0000ULL) >> 18);
-    /*
-    neighbours |= (discs & 0x7F7F7F7F7F7F7F7FULL) << 1;
-    neighbours |= (discs & 0xFEFEFEFEFEFEFEFEULL) >> 1;
-    neighbours |= (discs & 0x00FFFFFFFFFFFFFFULL) << 8;
-    neighbours |= (discs & 0xFFFFFFFFFFFFFF00ULL) >> 8;
-    neighbours |= (discs & 0x00FEFEFEFEFEFEFEULL) << 7;
-    neighbours |= (discs & 0x7F7F7F7F7F7F7F00ULL) >> 7;
-    neighbours |= (discs & 0x007F7F7F7F7F7F7FULL) << 9;
-    neighbours |= (discs & 0xFEFEFEFEFEFEFE00ULL) >> 9;
-    */
     neighbours &= ~discs;
     uint64_t puttable = neighbours & ~seen_cells;
+    if (black_line_mirror(discs) == discs){
+        puttable ^= black_line_mirror(puttable) & puttable & 0xFFFEFCF8F0E0C080ULL;
+    }
+    if (white_line_mirror(discs) == discs){
+        puttable ^= white_line_mirror(puttable) & puttable & 0xFF7F3F1F0F070301ULL;
+    }
     if (puttable){
         for (uint_fast8_t cell = first_bit(&puttable); puttable; cell = next_bit(&puttable)){
             uint64_t cell_bit = 1ULL << cell;
@@ -580,19 +580,21 @@ int main(int argc, char* argv[]){
     // need 1 or more full line to cause gameover by draw  (because there are both black and white discs)
     const std::vector<Task> tasks = {
         {0x8040201008040201ULL | 0x0000001818000000ULL, 20}, // a1-h8
-        //{0x0000000000FF0000ULL | 0x0000001818000000ULL, 20}, // a6-h6
-        //{0x00000000FF000000ULL | 0x0000001818000000ULL, 20}, // a5-h5
+        /*
+        {0x0000000000FF0000ULL | 0x0000001818000000ULL, 20}, // a6-h6
+        {0x00000000FF000000ULL | 0x0000001818000000ULL, 20}, // a5-h5
         
-        //{0x0080402010080402ULL | 0x0000001818000000ULL, 20}, // a2-g8
+        {0x0080402010080402ULL | 0x0000001818000000ULL, 20}, // a2-g8
         
-        //{0x0000804020100804ULL | 0x0000001818000000ULL, 20}, // a3-f8
-        //{0x0000008040201008ULL | 0x0000001818000000ULL, 20}, // a4-e8
-        //{0x000000000000FF00ULL | 0x0000001818000000ULL, 20}, // a7-h7
-        //{0x00000000000000FFULL | 0x0000001818000000ULL, 20}, // a8-h8
-        //{0x0000000080402010ULL | 0x0000001818000000ULL, 18}, // a5-d8
-        //{0x0000000000804020ULL | 0x0000001818000000ULL, 18}, // a6-c8
-        //{0x0000000020408070ULL | 0x0000001818000000ULL, 20}, // a7-b8, b6-c5, c8-d8
-        //{0x0000000080808070ULL | 0x0000001818000000ULL, 20}  // a7-b8, a5-a6, c8-d8
+        {0x0000804020100804ULL | 0x0000001818000000ULL, 20}, // a3-f8
+        {0x0000008040201008ULL | 0x0000001818000000ULL, 20}, // a4-e8
+        {0x000000000000FF00ULL | 0x0000001818000000ULL, 20}, // a7-h7
+        {0x00000000000000FFULL | 0x0000001818000000ULL, 20}, // a8-h8
+        {0x0000000080402010ULL | 0x0000001818000000ULL, 18}, // a5-d8
+        {0x0000000000804020ULL | 0x0000001818000000ULL, 18}, // a6-c8
+        {0x0000000020408070ULL | 0x0000001818000000ULL, 20}, // a7-b8, b6-c5, c8-d8
+        {0x0000000080808070ULL | 0x0000001818000000ULL, 20}  // a7-b8, a5-a6, c8-d8
+        */
     };
     std::cout << "task list:" << std::endl;
     for (Task task: tasks){
